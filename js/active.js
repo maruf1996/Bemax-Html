64cf705823a6b6ead3a04e70367e442beaff4825
maruf1996 <maruf3813@gmail.com>
2024-02-21 15:09:31 +0600

    first commit

---
+++ js/active.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  $(".slider-active").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    navText: [
+      '<i class="fa-solid fa-angle-left"></i>',
+      '<i class="fa-solid fa-angle-right"></i>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      },
+    },
+  });
+});
